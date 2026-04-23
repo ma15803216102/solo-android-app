65c304cb968a6eab69b2e7e74f716f3e17795f40
ma15803216102 <ma15803216102@users.noreply.github.com>
2026-04-23 03:39:34 +0000

    fix: use custom window inset listener to forcefully push WebView out of safe areas

--- android/app/src/main/java/com/trae/solo/MainActivity.java
+++ android/app/src/main/java/com/trae/solo/MainActivity.java
@@ -1,5 +1,36 @@
 package com.trae.solo;
 
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.graphics.Insets;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onStart() {
+        super.onStart();
+        
+        // Run after Bridge initialization
+        View webView = this.bridge.getWebView();
+        if (webView != null) {
+            ViewCompat.setOnApplyWindowInsetsListener(webView, (v, windowInsets) -> {
+                Insets insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars() | WindowInsetsCompat.Type.displayCutout());
+                ViewGroup.MarginLayoutParams mlp = (ViewGroup.MarginLayoutParams) v.getLayoutParams();
+                
+                if (mlp != null) {
+                    // Force the top and bottom margins to exactly match the system bars and cutout
+                    mlp.topMargin = insets.top;
+                    mlp.bottomMargin = insets.bottom;
+                    mlp.leftMargin = insets.left;
+                    mlp.rightMargin = insets.right;
+                    v.setLayoutParams(mlp);
+                }
+                
+                return windowInsets;
+            });
+        }
+    }
+}
